2a1a88b5e05c311e1ad09834d98ff806adda857d
Thomas Pope <tpope0928@gmail.com>
2019-01-02 17:57:11 +0000

    Automatically backed up by Learn

--- bin/enviroment.rb
+++ bin/enviroment.rb
@@ -1,27 +1,7 @@
-require_relative 'environment'
+require 'bundler/setup'
+require 'base64'
 
-class SQLRunner
-  def initialize(db)
-    @db = db
-  end
+Bundler.require
 
-  def execute_schema_sql
-    sql = File.read("lib/schema.sql")
-    @db.execute_batch(sql)
-  end
-
-  def execute_insert_sql
-    sql = File.read("lib/insert.sql")
-    @db.execute_batch(sql)
-  end
-
-  def execute_update_sql
-    sql = File.read("lib/update.sql")
-    @db.execute_batch(sql)
-  end
-
-  def execute_data
-    sql = File.read('lib/data.sql')
-    @db.execute_batch(sql)
-  end
-end
+require_relative 'sql_runner'
+require_relative '../lib/querying.rb'
