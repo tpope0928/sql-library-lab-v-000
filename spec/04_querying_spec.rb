0fb213e098bd63d8147a277b612d484c9f7f145d
Thomas Pope <tpope0928@gmail.com>
2019-01-02 17:11:26 +0000

    Done.

--- spec/04_querying_spec.rb
+++ spec/04_querying_spec.rb
@@ -11,15 +11,15 @@ describe 'querying' do
   end
 
   it 'returns the name and motto of the character with the longest motto' do
-    expect(@db.execute(select_name_and_motto_of_char_with_longest_motto)).to eq([["Tyrion Lannister", "A Lannister always pays his debts"]])
+    expect(@db.execute(select_name_and_motto_of_char_with_longest_motto)).to eq([["Tyrion Lanister", "A Lanister always pays is debts"]])
   end
 
   it 'determines the most prolific species of characters and return its value and count' do
-    expect(@db.execute(select_value_and_count_of_most_prolific_species)).to eq([["human", 6]])
+    expect(@db.execute(select_value_and_count_of_most_prolific_species)).to eq([["human", 4]])
   end
 
   it "selects the authors names and their series' subgenres" do
-    expect(@db.execute(select_name_and_series_subgenres_of_authors)).to eq([["George R. R. Martin", "high fantasy"], ["Second Author", "dark fantasy"]])
+    expect(@db.execute(select_name_and_series_subgenres_of_authors)).to eq([["George R. R. Martin", "medieval"], ["Second Author", "space opera"]])
   end
 
   it 'selects the series title with the most characters that are the species "human"' do
@@ -27,6 +27,6 @@ describe 'querying' do
   end
 
   it 'selects all of the character names and their number of books they have appeared in, in descending order' do
-    expect(@db.execute(select_character_names_and_number_of_books_they_are_in)).to eq([["Jake Chambers",3], ["Eddie", 3],["Daenerys Targaryen", 3], ["Tyrion Lannister", 3], ["OY", 1], ["Roland", 1], ["Eddard Stark", 1], ["Lady", 1]])
+    expect(@db.execute(select_character_names_and_number_of_books_they_are_in)).to eq([["Character Three",3], ["Character Two", 3],["Daenerys Targaryen", 3], ["Tyrion Lanister", 3], ["Character Four", 1], ["Character One", 1], ["Eddard Stark", 1], ["Lady", 1]])
   end
 end
