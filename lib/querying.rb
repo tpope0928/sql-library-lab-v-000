7635d2de2cddd697bf407f4de10e4f50580c31b9
Thomas Pope <tpope0928@gmail.com>
2019-01-02 18:13:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,22 +1,22 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year 
-  FROM books 
-  WHERE series_id = 1 
+  "SELECT title, year
+  FROM books
+  WHERE series_id = 1
   ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto 
-  FROM characters 
-  ORDER BY LENGTH(motto) DESC 
+  "SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC
   LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) 
-  FROM characters 
-  GROUP BY species 
-  ORDER BY COUNT(species) DESC 
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
   LIMIT 1;"
 end
 
